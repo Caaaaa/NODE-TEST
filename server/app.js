b8ec7e28315fa31e7cf646df34027b5b61116748
Chensx <372569753@qq.com>
2018-04-11 10:22:54 +0800

    update
    aaa

--- server/app.js
+++ server/app.js
@@ -1,23 +1,16 @@
 'use strict'
-
 const path = require('path')
 const Koa = require('koa')
-const Router = require('koa-router')
 const convert = require('koa-convert')
 const views = require('koa-views')
 const koaStatic = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const http = require('http')
+
+const routers = require('./router/index')
 const hostname = '127.0.0.1'
 const port = 3000
 const app = new Koa()
-
-const router = new Router()
-router.get('/index', async (ctx, next) => {
-    await ctx.render('index.html')
-}).get('/about', async (ctx, next) => {
-    await ctx.render('about.html')
-})
 
 app.use(async (ctx, next) => {
     const start = new Date()
@@ -26,10 +19,11 @@ app.use(async (ctx, next) => {
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 app.use(bodyParser())
-app.use(convert(koaStatic(path.join(__dirname, '../static'))))
-app.use(views(path.join(__dirname, './views')))
+app.use(koaStatic(path.join(__dirname, './../static')))
+// app.use(views(path.join(__dirname, './views')))
+app.use(views(path.join(__dirname, './../views')))
 
-app.use(router.routes(), router.allowedMethods())
+app.use(routers.routes(), routers.allowedMethods())
 
 http.createServer(app.callback()).listen(port, hostname, () => {
     console.log(`server 运行在 https://${hostname}:${port}/`)
